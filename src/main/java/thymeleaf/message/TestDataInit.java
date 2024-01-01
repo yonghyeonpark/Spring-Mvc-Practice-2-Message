49a1494d9229ce433b7274f36d5a54558bd28a9d
yonghyeonpark <pyh4616@naver.com>
2024-01-02 02:31:50 +0900

    setUp

---
+++ src/main/java/thymeleaf/message/TestDataInit.java
@@ -0,0 +1,24 @@
+package thymeleaf.message;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import thymeleaf.message.domain.item.Item;
+import thymeleaf.message.domain.item.ItemRepository;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
